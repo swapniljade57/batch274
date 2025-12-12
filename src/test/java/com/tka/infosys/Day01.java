82a4f52a607a0d6df85cf76af94e00311db6c0bd
swapniljade57 <swapniljade112@gmail.com>
2025-12-12 18:09:05 +0530

    first commit

---
+++ src/test/java/com/tka/infosys/Day01.java
@@ -0,0 +1,12 @@
+package com.tka.infosys;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day01 {
+
+	public static void main(String[] args) {
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://practicetestautomation.com/practice-test-login/");
+	}
+
+}
